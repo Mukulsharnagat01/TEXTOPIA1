50a85d2bef1ba2fcda24f99343b48cba4a27fc9b
Mukulsharnagat01 <sharnagatmukul@gmail.com>
2025-10-15 02:34:59 +0530

    Update the hosting

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  envPrefix: ['VITE_', 'REACT_APP_']
+  envPrefix: ['VITE_', 'REACT_APP_'],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase from 500 to 1000 kB
+  }
 })
